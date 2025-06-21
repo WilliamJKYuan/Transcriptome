7df54daf70724313078ea225b8b46db93af497e5
WilliamJKYuan <89084524+WilliamJKYuan@users.noreply.github.com>
2025-06-21 16:41:45 +0800

    修改注释并修复goRich错误

--- go_annotation/go_anal.r
+++ go_annotation/go_anal.r
@@ -20,20 +20,17 @@ piif <- function(pkg){
 }
 #if (!require("BiocManager", quietly = TRUE))
     #install.packages("BiocManager")
-
+#安装并加载clusterProfiler
 #BiocManager::install("clusterProfiler")
-
-
-#加载clusterProfiler 
 library(clusterProfiler)
   
-#加载背景库文件，即我们的第二个文件 
+#加载背景库文件：genes.new.go.annotation
 go_anno <- read.delim('genes.new.go.annotation', header=FALSE, stringsAsFactors =FALSE) 
 names(go_anno) <- c('gene_id','ID')
 go_anno$gene_id <- sub("\\.p\\d+$", "", go_anno$gene_id)  # 移除.pX后缀
 print("背景库加载完成")  
 
-#加载GO注释描述，即我们的第三个文件 
+#加载GO注释描述：go_term.list
 go_class <- read.delim('go_term.list', header=FALSE, stringsAsFactors =FALSE) 
 names(go_class) <- c('ID','Description','Ontology')
 print("GO注释描述加载完成")  
@@ -74,19 +71,18 @@ barplot(go_rich,drop=T)
 output_file <- "GO_barplot.pdf"  # 可替换为 .png/.tiff/.svg 等
 print("成功输出")
 # 动态调整图像高度（基于条目数量）
-n_cats <- min(20, nrow(go_rich))  # 最多显示20条
-img_height <- max(4, n_cats * 0.5) # 基础高度4英寸，每条目增加0.5英寸
+n_cats <- min(20, nrow(go_rich))					# 最多显示20条
+img_height <- max(4, n_cats * 0.5)					# 基础高度4英寸，每条目增加0.5英寸
 # 打开图形设备
-pdf(output_file, width = 10, height = img_height)  # PDF格式
-# 绘制图形
+pdf(output_file, width = 10, height = img_height)	# PDF格式
 print(barplot(go_rich, drop = TRUE, 
               title = "GO Enrichment Analysis",
-              showCategory = n_cats))
+              showCategory = n_cats))				# 绘制图形
 dev.off()
 message("已保存GO富集条形图至: ", output_file)
 
 
-#如果要画GO的网络关系图，则需要借助topGO跟Rgraphviz 
+#如果要画GO的网络关系图，使用topGO&Rgraphviz 
 #BiocManager::install('topGO') 
 #BiocManager::install('Rgraphviz') 
 library(topGO) 
@@ -94,11 +90,16 @@ library(topGO)
 write.table(go_rich, 'go_tmp.txt', sep='\t', row.names = FALSE, quote = FALSE) 
 tmp <- read.delim('go_tmp.txt') 
 tmp <- merge(tmp, go_class[c('ID', 'Ontology')], by = 'ID') 
-tmp <- tmp[c(10,1:9)] 
+tmp <- tmp[!is.na(tmp$Ontology), ]					# 删除NA的行
+# 修改Ontology数据，Ontology列的全称转换为缩写（BP/MF/CC）方可进行比较
+tmp$Ontology <- factor(tmp$Ontology, 
+                       levels = c("biological_process", "molecular_function", "cellular_component"),
+                       labels = c("BP", "MF", "CC")) %>% as.character()
+tmp <- tmp[c(13,1:9)] 
 tmp <- tmp[order(tmp$pvalue),] 
 
 write.table(tmp, 'go_rich.significant.txt', sep = '\t', row.names = FALSE, quote = FALSE) 
 go_rich_BP <- go_rich 
-go_rich_BP@result <- go_rich_BP@result[as.vector(subset(tmp,  Ontology == 'biological process')$ID),] 
+go_rich_BP@result <- go_rich_BP@result[as.vector(subset(tmp,  Ontology == 'BP')$ID),] 
 go_rich_BP@ontology <- 'BP' 
 plotGOgraph(go_rich_BP)
