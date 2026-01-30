3bc05e0424e5242d170b90971b63a8c4db1622ad
WilliamJKYuan <89084524+WilliamJKYuan@users.noreply.github.com>
2026-01-30 16:28:12 +0800

    Add R Script

---
+++ fastaq.gz2Matrix/4_featureCounts.R
@@ -0,0 +1,28 @@
+#表达定量和标准化
+library(Rsubread)
+library(limma)
+library(edgeR)
+ 
+args <- commandArgs(trailingOnly = TRUE)
+ 
+bamFile <- args[1]
+gtfFile <- args[2]
+nthreads <- args[3]
+outFilePref <- args[4]
+ 
+outStatsFilePath <- paste(outFilePref, '.stat', sep = '')
+outCountsFilePath <- paste(outFilePref, '.count', sep = '')
+ 
+fCountsList = featureCounts(bamFile, annot.ext=gtfFile, isGTFAnnotationFile=TRUE, 
+                            nthreads=nthreads, isPairedEnd=TRUE)
+ 
+dgeList = DGEList(counts=fCountsList$counts, genes=fCountsList$annotation)
+fpkm = rpkm(dgeList, dgeList$genes$Length)
+tpm = exp(log(fpkm) - log(sum(fpkm)) + log(1e6))
+ 
+write.table(fCountsList$stat, outStatsFilePath, sep="\t", col.names=FALSE, row.names=FALSE, quote=FALSE)
+ 
+featureCounts = cbind(fCountsList$annotation[1], fCountsList$counts, fpkm, tpm)
+colnames(featureCounts) = c('gene_id', 'counts', 'fpkm', 'tpm')
+ 
+write.table(featureCounts, outCountsFilePath, sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
